102fab5f664bdc14d05daa22ce78effec72e4ad5
developer <x729-robots@yandex.ru>
2024-06-10 21:44:56 +0300

    /*/ move classes to separate files

--- statistics.cpp
+++ statistics.cpp
@@ -1,159 +1,24 @@
-#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <limits>
-#include <numeric>
-#include <string>
 #include <vector>
 
-class IStatistics {
-  public:
-    virtual ~IStatistics() {}
+#include "cmax.h"
+#include "cmean.h"
+#include "cmin.h"
+#include "cpctn.h"
+#include "cstddev.h"
+#include "istatistics.h"
 
-    virtual void update(double next) = 0;
-    virtual double eval() const = 0;
-    virtual const char* name() const = 0;
-};
-
-class Min : public IStatistics {
-  public:
-    Min(double m_val) : m_min{m_val} {}
-
-    virtual void update(double next) override {
-        if (next < m_min) {
-            m_min = next;
-        }
-    }
-
-    virtual double eval() const override {
-        return m_min;
-    }
-
-    virtual const char* name() const override {
-        return "min";
-    }
-
-  private:
-    double m_min;
-};
-
-class Max : public IStatistics {
-  public:
-    Max(double m_val) : m_max{m_val} {}
-
-    virtual void update(double next) override {
-        if (next > m_max) {
-            m_max = next;
-        }
-    }
-
-    virtual double eval() const override {
-        return m_max;
-    }
-
-    virtual const char* name() const override {
-        return "max";
-    }
-
-  private:
-    double m_max;
-};
-
-class Mean : public IStatistics {
-  public:
-    Mean(double m_val) : m_mean{m_val}, m_summ{m_val}, m_n{1} {}
-
-    virtual void update(double next) override {
-        m_summ += next;
-        m_n++;
-    }
-
-    virtual double eval() const override {
-        return m_summ / m_n;
-    }
-
-    virtual const char* name() const override {
-        return "mean";
-    }
-
-  private:
-    double m_mean;
-    double m_summ; // summ of all values
-    double m_n;    // number of all values
-};
-
-class StdDev : public IStatistics {
-  public:
-    StdDev(double val) : m_values{val} {}
-
-    virtual void update(double next) override {
-        m_values.push_back(next);
-    }
-
-    virtual double eval() const override {
-        double summ{std::accumulate(m_values.begin(), m_values.end(), 0.)};
-        double mean{summ / m_values.size()};
-        double summSquare{0};
-        for (auto val : m_values) {
-            summSquare += pow(val - mean, 2);
-        }
-        return sqrt(summSquare / m_values.size());
-    }
-
-    virtual const char* name() const override {
-        return "std";
-    }
-
-  private:
-    std::vector<double> m_values;
-    double m_stdDev;
-    double m_summ; // summ of all values
-    double m_n;    // number of all values
-};
-
-class PctN : public IStatistics {
-  public:
-    PctN(double val, int pctN) : m_values{val}, m_pctN{pctN} {}
-
-    virtual void update(double next) override {
-        m_values.push_back(next);
-    }
-
-    virtual double eval() const override {
-        std::vector<double> values{m_values};
-        std::sort(values.begin(), values.end());
-        // int pc_i = (round(1000*(double)m_values.size() *
-        // ((double)m_pctN/100.))/1000);
-        int pc_i = round(m_values.size() * m_pctN / 100.) - 1;
-        return (m_values[pc_i]);
-    }
-
-    virtual const char* name() const override {
-        std::string out = "Pct";
-        out += std::to_string(m_pctN);
-        return out.c_str();
-    }
-
-  private:
-    std::vector<double> m_values;
-    int m_pctN;
-    double m_summ; // summ of all values
-    double m_n;    // number of all values
-};
 int main() {
 
-    const size_t statistics_count = 6;
-    IStatistics* statistics[statistics_count];
-
-    double val = 0;
+    double val = 0;//TODO not very nice, don't want get first val separately
     std::cin >> val;
-    int i{0};
-    statistics[i++] = new Min{val};
-    statistics[i++] = new Max{val};
-    statistics[i++] = new Mean{val};
-    statistics[i++] = new StdDev{val};
-    statistics[i++] = new PctN{val, 90};
-    statistics[i++] = new PctN{val, 95};
+    IStatistics* statistics[] = {
+        new Min{val},    new Max{val},       new Mean{val},
+        new StdDev{val}, new CPctN{val, 90}, new CPctN{val, 95}};
+
+    const size_t statistics_count = sizeof(statistics)/sizeof(*statistics);
 
     while (std::cin >> val) {
         for (size_t i = 0; i < statistics_count; ++i) {
